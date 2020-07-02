c99283fabb1d99c4900a303cedeaad9c48677aae
Саша Воробьёв <avorobiev@deloitte.ru>
2020-07-02 22:00:22 +0300

    Добавил логгер

--- 02/main.go
+++ 02/main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
+	log.Println("Server started")
+
 	http.Handle("/", http.FileServer(http.Dir("./starting-files/")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
