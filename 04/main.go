55fb3e14660932b1f126defa2e725c0f3900882a
Саша Воробьёв <avorobiev@deloitte.ru>
2020-07-03 08:58:46 +0300

    Solved 04

---
+++ 04/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./starting-files/public/"))))
+	http.HandleFunc("/", displaytemp)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func displaytemp(rew http.ResponseWriter, req *http.Request) {
+	tmp, err := template.ParseFiles("./starting-files/templates/index.gohtml")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tmp.Execute(rew, nil)
+}
